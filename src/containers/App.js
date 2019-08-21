dd6be93d159bdaee92cb40c8d2e9e447986d34b8
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-08-20 21:54:36 -0400

    Revert "First Commit: installed google-maps-react module, setting markers on state"
    This reverts commit ff6cba17e310ca68ffaf75e18054c54486fd5b6b.
    "first commit"

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { fetchLocations } from "../helpers/api";
+import { fetchLocations } from '../helpers/api';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      markers: []
-    };
-
-    this.geocoder = new this.props.google.maps.Geocoder()
-  }
-
   componentDidMount() {
     fetchLocations()
-      .then(response => {
-        this.setState({markers: response})
-        console.log(response)
-      })
+      .then(response => console.log(response))
       .catch(error => console.log(error));
   }
 
   render() {
-    this.geocoder.geocode({address: "44 Butler Pl, Brooklyn, NY 11238"}, (results, status) => {console.log(status)})
-    return (
-      <div>
-        <h1>Locations Map Exercise!</h1>
-        <Map
-          google={this.props.google}
-          zoom={8}
-          // style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176 }}
-        >
-          <Marker />
-        </Map>
-      </div>
-    );
+    return <h1>Locations Map Exercise!</h1>;
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyC401UbpUdxh-L1Vvgo3Cv_s8wZDRnqPUM"
-})(App);
+export default App;
