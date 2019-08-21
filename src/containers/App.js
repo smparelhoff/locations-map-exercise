46cd93c87759e666abf6834e26c492802e1f0953
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-08-20 22:20:39 -0400

    Hooked up google maps API with google-maps-react module

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
 
-import { fetchLocations } from '../helpers/api';
+import {apiKey} from '../helpers/secrets';
+import { fetchLocations } from "../helpers/api";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: []
+    };
+
+    this.decoder = new this.props.google.maps.Geocoder()
+  }
+
   componentDidMount() {
     fetchLocations()
-      .then(response => console.log(response))
+      .then(response => {
+        console.log(response);
+        this.setState({ markers: response });
+      })
       .catch(error => console.log(error));
   }
 
   render() {
-    return <h1>Locations Map Exercise!</h1>;
+    return (<div>
+      <h1>Locations Map Exercise!</h1>
+      <Map
+        google={this.props.google}
+        zoom={8}
+        initialCenter={{lat: 47.444, lng: -122.176}}
+        >
+        <Marker />
+      </Map>
+      </div>)
   }
 }
 
-export default App;
+export default GoogleApiWrapper({apiKey})(App);
